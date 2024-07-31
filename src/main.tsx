98c84cf5717267dae59fe512c112f9276e3b6e82
zhourengui <729049182@qq.com>
2024-07-31 20:56:09 +0800

    Clear code

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,8 @@
-import RootComponent from "./root-component.tsx";
-import { createRoot } from "react-dom/client";
-import { reactBridge } from "@garfish/bridge-react-v18";
+import RootComponent from './root-component.tsx';
+import { createRoot } from 'react-dom/client';
 
-import "./index.css";
+import './index.css';
 
-export const provider = reactBridge({
-  el: "#root",
-  rootComponent: RootComponent,
-  errorBoundary: () => <>micro-react error</>,
-});
-
-if (!window.__GARFISH__) {
-  const container = document.getElementById("root");
-  const root = createRoot(container!);
-  root.render(<RootComponent basename={"/"} />);
-}
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(<RootComponent basename={'/'} />);
