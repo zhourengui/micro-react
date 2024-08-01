629bff18c0223f54724945f9328dcab66fbf2412
zhourengui <Nr@*y3FsBGg2>
2024-08-01 11:29:45 +0800

    Clear code

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "http://localhost:5173/",
   server: {
     port: 5173,
     cors: true,
-    origin: "http://localhost:5173",
-  },
-  build: {
-    lib: {
-      entry: 'src/main.tsx',
-      formats: ['umd'],
-    },
   },
 });
